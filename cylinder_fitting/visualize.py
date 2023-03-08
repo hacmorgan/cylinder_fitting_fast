8bc9f9e39f4e27621840eebec9effba51a417579
Hamish Morgan <ham430@gmail.com>
2023-03-08 11:02:15 +1100

    Autoformat with black

--- cylinder_fitting/visualize.py
+++ cylinder_fitting/visualize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
@@ -20,15 +21,18 @@ def set_axes_equal(ax: plt.Axes):
     spheres and cubes as cubes.  Required since `ax.axis('equal')`
     and `ax.set_aspect('equal')` don't work on 3D.
     """
-    limits = np.array([
-        ax.get_xlim3d(),
-        ax.get_ylim3d(),
-        ax.get_zlim3d(),
-    ])
+    limits = np.array(
+        [
+            ax.get_xlim3d(),
+            ax.get_ylim3d(),
+            ax.get_zlim3d(),
+        ]
+    )
 
     origin = np.mean(limits, axis=1)
     radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
     _set_axes_radius(ax, origin, radius)
+
 
 def _set_axes_radius(ax, origin, radius):
     x, y, z = origin
@@ -38,7 +42,7 @@ def _set_axes_radius(ax, origin, radius):
 
 
 def show_G_distribution(data):
-    '''Show the distribution of the G function.'''
+    """Show the distribution of the G function."""
     Xs, t = fitting.preprocess_data(data)
 
     Theta, Phi = np.meshgrid(np.linspace(0, np.pi, 50), np.linspace(0, 2 * np.pi, 50))
@@ -50,17 +54,18 @@ def show_G_distribution(data):
             w = fitting.direction(Theta[i][j], Phi[i][j])
             G[-1].append(fitting.G(w, Xs))
 
-    plt.imshow(G, extent=[0, np.pi, 0, 2 * np.pi], origin='lower')
+    plt.imshow(G, extent=[0, np.pi, 0, 2 * np.pi], origin="lower")
     plt.show()
 
+
 def show_fit(w_fit, C_fit, r_fit, Xs):
-    '''Plot the fitting given the fitted axis direction, the fitted
+    """Plot the fitting given the fitted axis direction, the fitted
     center, the fitted radius and the data points.
-    '''
+    """
     data = np.array(Xs)
 
     fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    ax = fig.gca(projection="3d")
 
     # Plot the data points
 
@@ -71,17 +76,19 @@ def show_fit(w_fit, C_fit, r_fit, Xs):
     theta = np.arccos(np.dot(w_fit, np.array([0, 0, 1])))
     phi = np.arctan2(w_fit[1], w_fit[0])
 
-    M = np.dot(rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
-               rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta))
+    M = np.dot(
+        rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
+        rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta),
+    )
 
-    xd = np.max(data[:,0]) - np.min(data[:,0])
-    yd = np.max(data[:,1]) - np.min(data[:,1])
-    zd = np.max(data[:,2]) - np.min(data[:,2])
-    c = np.sqrt(xd**2+zd**2+yd**2)
+    xd = np.max(data[:, 0]) - np.min(data[:, 0])
+    yd = np.max(data[:, 1]) - np.min(data[:, 1])
+    zd = np.max(data[:, 2]) - np.min(data[:, 2])
+    c = np.sqrt(xd**2 + zd**2 + yd**2)
 
     # Plot the cylinder surface
     delta = np.linspace(-np.pi, np.pi, 20)
-    z = np.linspace(-c/2, c/2, 20)
+    z = np.linspace(-c / 2, c / 2, 20)
 
     Delta, Z = np.meshgrid(delta, z)
     X = r_fit * np.cos(Delta)
@@ -95,18 +102,27 @@ def show_fit(w_fit, C_fit, r_fit, Xs):
             Y[i][j] = p[1]
             Z[i][j] = p[2]
 
-    ax.plot_surface(X, Y, Z, alpha=0.5, color='green')
+    ax.plot_surface(X, Y, Z, alpha=0.5, color="green")
 
     # Plot the center and direction
-    ax.quiver(C_fit[0], C_fit[1], C_fit[2],
-            r_fit * w_fit[0], r_fit * w_fit[1], r_fit * w_fit[2], color='red')
+    ax.quiver(
+        C_fit[0],
+        C_fit[1],
+        C_fit[2],
+        r_fit * w_fit[0],
+        r_fit * w_fit[1],
+        r_fit * w_fit[2],
+        color="red",
+    )
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
 
     # plt.axis('equal')
-    ax.set_box_aspect([1,1,1]) # IMPORTANT - this is the new, key line
-    ax.set_proj_type('ortho') # OPTIONAL - default is perspective (shown in image above)
-    set_axes_equal(ax) # IMPORTANT - this is also required
+    ax.set_box_aspect([1, 1, 1])  # IMPORTANT - this is the new, key line
+    ax.set_proj_type(
+        "ortho"
+    )  # OPTIONAL - default is perspective (shown in image above)
+    set_axes_equal(ax)  # IMPORTANT - this is also required
     plt.show()
--- test_fitting.py
+++ test_fitting.py
@@ -2,7 +2,8 @@
 
 import pytest
 import numpy as np
-np.seterr(all='raise')
+
+np.seterr(all="raise")
 
 from cylinder_fitting import fit
 from cylinder_fitting import show_fit
@@ -10,15 +11,18 @@ from cylinder_fitting import show_G_distribution
 from cylinder_fitting import geometry
 from cylinder_fitting import fitting_rmsd
 from cylinder_fitting import distance_to_cylinder
+
 # import open3d as o3d
 
 
 def make_points_on_a_cylinder(theta, phi, C, r, N):
-    '''Make N points on a cylinder defined by the center C, direction defined theta and phi and radius r.
-    Also return the direction of the cylinder'''
+    """Make N points on a cylinder defined by the center C, direction defined theta and phi and radius r.
+    Also return the direction of the cylinder"""
 
-    M = np.dot(geometry.rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
-               geometry.rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta))
+    M = np.dot(
+        geometry.rotation_matrix_from_axis_and_angle(np.array([0, 0, 1]), phi),
+        geometry.rotation_matrix_from_axis_and_angle(np.array([0, 1, 0]), theta),
+    )
 
     x = np.dot(M, np.array([1, 0, 0]))
     y = np.dot(M, np.array([0, 1, 0]))
@@ -27,24 +31,32 @@ def make_points_on_a_cylinder(theta, phi, C, r, N):
     delta = np.radians(10)
     t = 0.1
 
-    return [C + r * (np.cos(i * delta) * x + np.sin(i * delta) * y + i * t * z) for i in range(N)], z
+    return [
+        C + r * (np.cos(i * delta) * x + np.sin(i * delta) * y + i * t * z)
+        for i in range(N)
+    ], z
+
 
 def load_point_cloud_txt(filename):
     points = []
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         line = f.readline()
         while line:
-            tokens = line.split(' ')
-            points.append(np.array([float(tokens[0]), float(tokens[1]), float(tokens[2])]))
+            tokens = line.split(" ")
+            points.append(
+                np.array([float(tokens[0]), float(tokens[1]), float(tokens[2])])
+            )
 
             line = f.readline()
 
     return points
 
+
 # def load_point_cloud_ply(filename):
 #     pc = o3d.io.read_point_cloud(filename)
 
 #     return pc.points
+
 
 def test_fit():
     print("test fit.")
@@ -53,11 +65,11 @@ def test_fit():
     r = 10
     # data, w = make_points_on_a_cylinder(-0.8, 0.2, C, r, 100)
 
-    data = load_point_cloud_txt('data/cylinder01_xyz.txt')
+    data = load_point_cloud_txt("data/cylinder01_xyz.txt")
 
     w_fit, C_fit, r_fit, fit_err = fit(data)
 
-    #show_G_distribution(data)
+    # show_G_distribution(data)
 
     print("Fitting RMSD =", fitting_rmsd(w_fit, C_fit, r_fit, data))
 
@@ -67,5 +79,6 @@ def test_fit():
     # assert(np.absolute(np.dot(w, C - C_fit)) / np.linalg.norm(w) / np.linalg.norm(C - C_fit) > 0.999)
     # assert(np.absolute(r - r_fit) < r_fit * 1e-4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_fit()
