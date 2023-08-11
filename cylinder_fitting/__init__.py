cc88a82f678628a968e5a5e9c3de961d66b4adef
Hamish Morgan <ham430@gmail.com>
2023-08-11 14:29:53 +1000

    Remove visualisation library from __init__

--- cylinder_fitting/__init__.py
+++ cylinder_fitting/__init__.py
@@ -1,7 +1,4 @@
 from .fitting import fit
 
-from .visualize import show_fit
-from .visualize import show_G_distribution
-
 from .analysis import fitting_rmsd
 from .analysis import distance_to_cylinder
